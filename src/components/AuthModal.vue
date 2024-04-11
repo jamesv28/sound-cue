b4ddddb1dab2f6dc55bdbf6d43242fff910aa0c8
James Volmert <james.volmert@gmail.com>
2024-04-10 18:55:19 -0600

    form validation on registration form

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,12 +1,27 @@
 <script>
 import { mapState, mapWritableState } from 'pinia'
 import { useModalStore } from '@/stores/modals'
+import { errorMessages } from 'vue/compiler-sfc'
 
 export default {
   name: 'AuthModal',
   data() {
     return {
-      tab: 'login'
+      tab: 'login',
+      schema: {
+        name: 'required|min:3|max:100|alpha_spaces',
+        email: 'required|email|min:3|max:100',
+        age: 'required|min_value:18|max_value:100',
+        password: 'required|min:3|max:100|excluded:password',
+        confirmPassword: 'required|confirmed:@password',
+        country: 'required',
+        tos: 'required'
+      }
+    }
+  },
+  methods: {
+    register(values) {
+      console.log(values)
     }
   },
   computed: {
@@ -105,80 +120,102 @@ export default {
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-else>
+          <vee-form v-else :validation-schema="schema" @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2" for="name">Name</label>
-              <input
+              <vee-field
+                name="name"
                 id="name"
                 type="text"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <error-message class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2" for="rEmail">Email</label>
-              <input
+              <vee-field
+                name="email"
                 id="rEmail"
                 type="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <error-message class="text-red-600" name="email" />
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2" for="age">Age</label>
-              <input
+              <vee-field
+                name="age"
                 id="age"
                 type="number"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
+              <error-message class="text-red-600" name="age" />
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2" for="rPassword">Password</label>
-              <input
-                id="rPassword"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
+              <vee-field name="password" id="rPassword" :bails="false" v-slot="{ field, errors }">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error"></div>
+              </vee-field>
+              <error-message class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2" for="crPassword">Confirm Password</label>
-              <input
+              <vee-field
+                name="confirmPassword"
                 id="crPassword"
                 type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
+              <error-message class="text-red-600" name="confirmPassword" />
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2" for="country">Country</label>
-              <select
+              <vee-field
+                as="select"
+                name="country"
                 id="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               >
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+              </vee-field>
+              <error-message class="text-red-600" name="country" />
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" id="accept" />
+              <vee-field
+                name="tos"
+                type="checkbox"
+                class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+                id="accept"
+                value="1"
+              />
               <label class="inline-block" for="accept">Accept terms of service</label>
             </div>
+            <error-message class="text-red-600" name="tos" />
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
